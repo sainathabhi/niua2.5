0836fc9e582589481e184b668549b2d96879bbb1
krishna-pwc <krishinbox2@gmail.com>
2020-05-20 00:00:52 +0530

    NULP pages are removed.

--- src/app/client/src/app/modules/public/public-routing.module.ts
+++ src/app/client/src/app/modules/public/public-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './components';
 import { LandingpageGuard } from './services';
 import { OfflineApplicationDownloadComponent } from '@sunbird/shared';
-import { NuihComponent, LearnComponent, InnovateComponent, DataExchangeComponent, SmartGovComponent, AboutusComponent, ComingSoonComponent } from './module/nuih';
+
 const routes: Routes = [
   {
     path: '', component: LandingPageComponent, canActivate: [LandingpageGuard],
@@ -11,55 +11,6 @@ const routes: Routes = [
   },
   {
     path: 'explore', loadChildren: './module/explore/explore.module#ExploreModule'
-  },
-  {
-    path: 'nulp', component: NuihComponent, data: {
-      telemetry: {
-        env: 'public', pageid: 'home', type: 'view', subtype: 'paginate'
-      }
-    }
-  },
-  {
-    path: 'lms', component: LearnComponent, data: {
-      telemetry: {
-        env: 'public', pageid: 'lms', type: 'view', subtype: 'paginate'
-      }
-    }
-  },
-  {
-    path: 'innovate', component: InnovateComponent, data: {
-      telemetry: {
-        env: 'public', pageid: 'innovate', type: 'view', subtype: 'paginate'
-      }
-    }
-  },
-  {
-    path: 'iudx', component: DataExchangeComponent, data: {
-      telemetry: {
-        env: 'public', pageid: 'iudx', type: 'view', subtype: 'paginate'
-      }
-    }
-  },
-  {
-    path: 'smartgov', component: SmartGovComponent, data: {
-      telemetry: {
-        env: 'public', pageid: 'smartgov', type: 'view', subtype: 'paginate'
-      }
-    }
-  },
-  {
-    path: 'aboutus', component: AboutusComponent, data: {
-      telemetry: {
-        env: 'public', pageid: 'aboutus', type: 'view', subtype: 'paginate'
-      }
-    }
-  },
-  {
-    path: 'comingsoon', component: ComingSoonComponent, data: {
-      telemetry: {
-        env: 'public', pageid: 'comingsoon', type: 'view', subtype: 'paginate'
-      }
-    }
   },
   {
     path: ':slug/explore', loadChildren: './module/explore/explore.module#ExploreModule'
--- src/app/client/src/app/modules/public/public.module.ts
+++ src/app/client/src/app/modules/public/public.module.ts
@@ -6,15 +6,12 @@ import { PublicPlayerService, LandingpageGuard } from './services';
 import { SharedModule } from '@sunbird/shared';
 import { PublicRoutingModule } from './public-routing.module';
 import { DeviceDetectorService } from 'ngx-device-detector';
-import { NuihModule } from './module/nuih/nuih.module';
 @NgModule({
   imports: [
     CommonModule,
     CoreModule,
     SharedModule,
-    PublicRoutingModule,
-    // #NULP change: Imported NuihModule
-    NuihModule
+    PublicRoutingModule
   ],
   declarations: [LandingPageComponent],
   providers: [PublicPlayerService, DeviceDetectorService, LandingpageGuard]
